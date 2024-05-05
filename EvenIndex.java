3ae81dad87a05722e4ae7a193ae1ff52bf6d41b4
Gayathri <161328940+Gayathrii08@users.noreply.github.com>
2024-05-05 19:40:14 +0530

    Update EvenIndex.java

--- EvenIndex.java
+++ EvenIndex.java
@@ -5,8 +5,8 @@ public class EvenIndex {
                           int sum = 0;
                          for (int i = 0;i<a.length;i++)
                         {
-                          if(i%2 != 0)
-                          sum += + i;
+                          if(i%2 == 0)
+                          sum += i;
                          }
                   System.out.println(sum);
                        }
