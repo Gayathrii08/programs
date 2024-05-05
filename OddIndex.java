d467f749ae2d29d882f28b3dc76a556748e70716
Gayathri <161328940+Gayathrii08@users.noreply.github.com>
2024-05-05 19:33:16 +0530

    Add files via upload
    Program to print the sum of odd index in an array

---
+++ OddIndex.java
@@ -0,0 +1,13 @@
+public class OddIndex {
+                  public static void main(String args[])
+                        {
+                          int a[] = {1,2,3,4};
+                          int sum = 0;
+                         for (int i = 0;i<a.length;i++)
+                        {
+                          if(i%2 != 0)
+                          sum += + i;
+                         }
+                  System.out.println(sum);
+                       }
+                     }
